4b360a4d35afe7362641ead459eb678ce6a5cd6d
Mega2223 <59067466+Mega2223@users.noreply.github.com>
2024-09-30 00:35:15 -0300

    NeonFC

--- util/render.js
+++ util/render.js
@@ -51,7 +51,7 @@ class Renderable {
         gl.bindBuffer(gl.ARRAY_BUFFER, this.color_vbo)
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.colors), gl.STATIC_DRAW);
     }
-    draw(projMat, aspect = 1){
+    draw(projMat, aspect = 1, mode = gl.LINES){
         gl.useProgram(this.shaderProg);
         gl.uniform4f(this.coord_vec_loc,this.coords[0],this.coords[1],this.coords[2],this.coords[3])
         gl.uniformMatrix4fv(this.proj_mat_loc,false,projMat);
@@ -62,7 +62,7 @@ class Renderable {
         gl.vertexAttribPointer(1, 3, gl.FLOAT, false, 0, 0);
         gl.bindBuffer(gl.ARRAY_BUFFER, this.vert_vbo)
         gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
-        gl.drawArrays(gl.LINES, 0, this.vertices.length/3);
+        gl.drawArrays(mode, 0, this.vertices.length/3);
         gl.disableVertexAttribArray(1);
         gl.disableVertexAttribArray(0);
     }
