1e9f3e9a379a95a62c96aa08272205df599528e0
mega2223 <juliocesarborgesdeoliveir@gmail.com>
2026-06-07 22:45:58 -0300

    paginação funcional

--- projetos.js
+++ projetos.js
@@ -2,24 +2,32 @@
 console.log('oi')
 
 const pages = {
-	'a': document.getElementById('a'),
-	'b': document.getElementById('b'),
-	'c': document.getElementById('c')
+	'AguaEngine3D': document.getElementById('a'),
+	'NeonFC': document.getElementById('b'),
+	'SimpleFileTransferer': document.getElementById('c'),
+	'NeveAnalytics': document.getElementById('d'),
+	'Readify': document.getElementById('e')
 };
+const selectors = {};
 
 var current = null;
+var currentSelector = null;
 
 function setCurrentPage(index) {
 	if (pages[index] == null) {
 		console.warn(`PAGINA ${index} NAO EXISTE`);
-		return 0;
+		return;
 	} 
 	if (current != null) {
 		current.style.visibility = 'hidden';
+		currentSelector.classList.remove("selected");
 	}
 	current = pages[index];
 	console.debug(pages);
 	current.style.visibility = 'visible';
+	currentSelector = selectors[index];
+	currentSelector.classList.add("selected");
+	console.debug('D=',selectors[index]);
 }
 
 setCurrentPage('a');
@@ -29,12 +37,14 @@ const selector = document.getElementById('selector')
 for (const [key, value] of Object.entries(pages)) {
 	console.log(key, '= ->', value);
 	const div = document.createElement('div');
+	const div_id = `${key}-sel`
 	div.innerHTML = `
-		<div>
-			<p onclick="setCurrentPage('${key}')">${key}</p>
+		<div id=${div_id}>
+			<p onclick="setCurrentPage('${key}')">\> ${key}</p>
 		</div>
 	`;
 	selector.appendChild(div);
+	selectors[key] = div;
 }
 
 
